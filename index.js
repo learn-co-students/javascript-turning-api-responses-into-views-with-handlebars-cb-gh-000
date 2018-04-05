fcb698bf1d62866c4e11be5b59c8580e8406a005
norman lewis <takialog@gmail.com>
2018-04-05 14:33:45 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const src = document.getElementById('repository-template').innerHTML
+  const templates = Handlebars.compile(src)
+  const repoList = templates(repos)
+
+  document.getElementById("repositories").innerHTML = repoList
+  console.log(repos)
+}
